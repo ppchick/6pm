12fc5483500dcda1c3ecfe157ba15e12c3e1e491
guomk <gmk19990312@gmail.com>
2019-03-15 12:16:36 +0800

    fix pubspec.lock

--- sixpm_gym/lib/pages/welcome/welcome.dart
+++ sixpm_gym/lib/pages/welcome/welcome.dart
@@ -182,7 +182,7 @@ class _MyHomePageState extends State<WelcomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                // NOTE Button
+                // ANCHOR  Button for test easy navigation
                 ButtonBar(
                     alignment: MainAxisAlignment.center,
                     children: <Widget>[
