418375f15eaa6eec070fde429ca46c2563ecd12b
Limzui <37795828+Limzui@users.noreply.github.com>
2019-03-19 00:04:33 +0800

    Created skeleton join session page

--- sixpm_gym/lib/pages/session/createSession.dart
+++ sixpm_gym/lib/pages/session/createSession.dart
@@ -15,7 +15,7 @@ class CreateSessionState extends State<CreateSessionPage> {
         children: <Widget>[
           Container(
             padding: EdgeInsets.fromLTRB(50.0, 50.0, 0.0, 0.0),
-            child: Text('Create A Session',
+            child: Text('Create A Session!',
                 style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
           ),
         ],
