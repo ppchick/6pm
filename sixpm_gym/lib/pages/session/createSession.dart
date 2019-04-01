d28663b9f5378d9a42911e840d546dd91941a51a
Limzui <37795828+Limzui@users.noreply.github.com>
2019-04-02 00:05:26 +0800

    Revamped checkin page

--- sixpm_gym/lib/pages/session/createSession.dart
+++ sixpm_gym/lib/pages/session/createSession.dart
@@ -13,52 +13,52 @@ class CreateSessionState extends State<CreateSession> {
   String _startTime, _endTime, _location = 'SEARCH FOR GYM';
   int _startTimeIndex, _endTimeIndex;
   List<String> time = [
-    '00:00',  //0
+    '00:00', //0
     '00:30',
     '01:00',
     '01:30',
     '02:00',
-    '02:30',  //5
+    '02:30', //5
     '03:00',
     '03:30',
     '04:00',
     '04:30',
-    '05:00',  //10
+    '05:00', //10
     '05:30',
     '06:00',
     '06:30',
     '07:00',
-    '07:30',  //15
+    '07:30', //15
     '08:00',
     '08:30',
     '09:00',
     '09:30',
-    '10:00',  //20
+    '10:00', //20
     '10:30',
     '11:00',
     '11:30',
     '12:00',
-    '12:30',  //25
+    '12:30', //25
     '13:00',
     '13:30',
     '14:00',
     '14:30',
-    '15:00',  //30
+    '15:00', //30
     '15:30',
     '16:00',
     '16:30',
     '17:00',
-    '17:30',  //35
+    '17:30', //35
     '18:00',
     '18:30',
     '19:00',
     '19:30',
-    '20:00',  //40
+    '20:00', //40
     '20:30',
     '21:00',
     '21:30',
     '22:00',
-    '22:30',  //45
+    '22:30', //45
     '23:00',
     '23:30',
   ];
@@ -84,25 +84,46 @@ class CreateSessionState extends State<CreateSession> {
     );
   }
 
-  void _timeErrorDialog(context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Error!"),
-          content: new Text("End time must be after start time!"),
-          actions: <Widget>[
-            new FlatButton(
-              child: new Text("Okay"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+  void _timeErrorDialog(context, int reason) {
+    if (reason == 1) {  //End time must be after start time
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: new Text("Error!"),
+            content: new Text("End time must be after start time!"),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {            //Start date/time must be after now
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: new Text("Error!"),
+            content: new Text("Start date/time cannot be in the past!"),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text("Okay"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
@@ -144,7 +165,7 @@ class CreateSessionState extends State<CreateSession> {
 
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> params = [];
-
+    DateTime now = DateTime.now();
     return new Scaffold(
       resizeToAvoidBottomInset: false,
       body: Column(
@@ -346,7 +367,10 @@ class CreateSessionState extends State<CreateSession> {
                             _errorDialog(context);
                           } else if (_startTimeIndex >= _endTimeIndex) {
                             //start time after end time
-                            _timeErrorDialog(context);
+                            _timeErrorDialog(context, 1);
+                          } else if (DateTime.parse(globals.dateISO).isBefore(DateTime.now())) {
+                            //start date before now
+                            _timeErrorDialog(context, 2);
                           } else {
                             String _startDateTime =
                                 globals.dateISO + ' ' + _startTime + ':00';
@@ -358,7 +382,10 @@ class CreateSessionState extends State<CreateSession> {
                             params.add({'endTime': _endTime});
                             params.add({'date': globals.date});
                             params.add({'startDateTimeISO': startDateTime});
-                            params.add({'numHour': ((_endTimeIndex - _startTimeIndex)*0.5)});
+                            params.add({
+                              'numHour':
+                                  ((_endTimeIndex - _startTimeIndex) * 0.5)
+                            });
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
